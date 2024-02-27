cfed1e9d6fe31e0911e4f4594dff2f78c91edaf6
Shahzad Qadir <shahzadqadir@hotmail.co.uk>
2024-02-27 08:49:28 +0000

    update settings with new environment variables, updated gitignore file

--- personaltracker/settings.py
+++ personaltracker/settings.py
@@ -87,10 +87,10 @@ WSGI_APPLICATION = 'personaltracker.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',        
-        'NAME': 'postgres',
-        'HOST': 'db',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres'
+        'NAME': os.environ.get("DB_NAME"),
+        'HOST': os.environ.get("DB_HOST"),
+        'USER': os.environ.get("DB_USER"),
+        'PASSWORD': os.environ.get("DB_PASSWORD")
     }
 }
 
